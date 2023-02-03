824c89ffb0d321e8220efd8ea8b562cfc18b5165
izo0619 <isabelzhong30@gmail.com>
2023-02-02 23:54:22 -0600

    shortened to match bamocar docs

--- include/inverter_driver.h
+++ include/inverter_driver.h
@@ -27,9 +27,9 @@ class Inverter : public IInverter
 {
     Inverter(ICAN &can_interface_): can_interface_(can_interface_){};
 
-    virtual float GetMotorTemperature(){};
-    virtual float GetInverterTemperature(){};
-    virtual float GetRPM(){};
+    virtual float GetMotorTemperature() override ;
+    virtual float GetInverterTemperature() override ;
+    virtual float GetRPM() override;
     virtual void GetStatus() override;
     virtual void RequestTorque(float percent) override;
     virtual void RXCallback();
@@ -48,11 +48,8 @@ class Inverter : public IInverter
         CANSignal<uint8_t, 16, 8, CANTemplateConvertFloat(1), CANTemplateConvertFloat(0)> t_byte_2{};
         CANSignal<uint8_t, 24, 8, CANTemplateConvertFloat(1), CANTemplateConvertFloat(0)> t_byte_3{};
         CANSignal<uint8_t, 32, 8, CANTemplateConvertFloat(1), CANTemplateConvertFloat(0)> t_byte_4{};
-        CANSignal<uint8_t, 40, 8, CANTemplateConvertFloat(1), CANTemplateConvertFloat(0)> t_byte_5{};
-        CANSignal<uint8_t, 48, 8, CANTemplateConvertFloat(1), CANTemplateConvertFloat(0)> t_byte_6{};
-        CANSignal<uint8_t, 56, 8, CANTemplateConvertFloat(1), CANTemplateConvertFloat(0)> t_byte_7{};
-
-        CANTXMessage<8> Transmission_Msg{can_interface_, kTransmissionId, 8, 100, t_regid, t_byte_1, t_byte_2, t_byte_3, t_byte_4, t_byte_5, t_byte_6, t_byte_7};
+        
+        CANTXMessage<5> Transmission_Msg{can_interface_, kTransmissionId, 8, 100, t_regid, t_byte_1, t_byte_2, t_byte_3, t_byte_4};
         
         CANSignal<uint8_t, 0, 8, CANTemplateConvertFloat(1), CANTemplateConvertFloat(0)> r_regid{};
         CANSignal<uint8_t, 8, 8, CANTemplateConvertFloat(1), CANTemplateConvertFloat(0)> r_byte_1{};
@@ -60,10 +57,8 @@ class Inverter : public IInverter
         CANSignal<uint8_t, 24, 8, CANTemplateConvertFloat(1), CANTemplateConvertFloat(0)> r_byte_3{};
         CANSignal<uint8_t, 32, 8, CANTemplateConvertFloat(1), CANTemplateConvertFloat(0)> r_byte_4{};
         CANSignal<uint8_t, 40, 8, CANTemplateConvertFloat(1), CANTemplateConvertFloat(0)> r_byte_5{};
-        CANSignal<uint8_t, 48, 8, CANTemplateConvertFloat(1), CANTemplateConvertFloat(0)> r_byte_6{};
-        CANSignal<uint8_t, 56, 8, CANTemplateConvertFloat(1), CANTemplateConvertFloat(0)> r_byte_7{};
 
-        CANRXMessage<8> Receive_Msg{can_interface_, kReceiveId, RXCallback, r_regid, r_byte_1, r_byte_2, r_byte_3, r_byte_4, r_byte_5, r_byte_6, r_byte_7};
+        CANRXMessage<6> Receive_Msg{can_interface_, kReceiveId, RXCallback, r_regid, r_byte_1, r_byte_2, r_byte_3, r_byte_4, r_byte_5};
 
         enum class regId:uint8_t 
         {
--- src/inverter_driver.cpp
+++ src/inverter_driver.cpp
@@ -2,13 +2,10 @@
 
 void Inverter::RXCallback()
 {
-    float value = static_cast<uint8_t>(r_byte_1 << 48 | 
-                                        r_byte_2 << 40 | 
-                                        r_byte_3 << 32 | 
-                                        r_byte_4 << 24 | 
-                                        r_byte_5 << 16 | 
-                                        r_byte_6 << 8 | 
-                                        r_byte_7);
+    float value = static_cast<uint8_t>(r_byte_1 | 
+                                        r_byte_2 << 8 | 
+                                        r_byte_3 << 16 | 
+                                        r_byte_4 << 24);
     switch (r_regid){
         case static_cast<uint8_t>(regId::T_MOTOR):
             motor_temp = value;
@@ -17,21 +14,31 @@ void Inverter::RXCallback()
             inverter_temp = value;
             break;    
     }
-}
+};
+
+float Inverter::GetMotorTemperature(){
+    //bruh
+};
+
+float Inverter::GetInverterTemperature(){
+    //bruh
+};
+
+float Inverter::GetRPM(){
+    //bruh
+};
+
 void Inverter::GetStatus(){
     //bruh
-}
+};
 
 void Inverter::RequestTorque(float percent)
 {
     int16_t value = percent * 32766;
-    t_byte_1 = (value>>8) & 0xFF;
-    t_byte_2 = (value>>0) & 0xFF;
+    t_byte_1 = (value>>0) & 0xFF;
+    t_byte_2 = (value>>8) & 0xFF;
     t_byte_3 = 0;
     t_byte_4 = 0;
-    t_byte_5 = 0;
-    t_byte_6 = 0;
-    t_byte_7 = 0;
     t_regid = static_cast<uint8_t>(regId::TORQUE_SETPOI);
     Transmission_Msg.EncodeAndSend();
-}
+};
